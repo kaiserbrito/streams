63ef80871fd0edacf853e971e12863fdeebdd0c6
Victor Guerra <victorbcguerra@gmail.com>
2020-06-14 18:06:38 +0100

    Defined method to check if it was the current user who created the data

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -4,6 +4,17 @@ class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
+
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  }
 };
 
 export default StreamList;

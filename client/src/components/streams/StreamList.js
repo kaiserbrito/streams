252b8f082d9b9ea1ee61f22e317fd1d5129d5896
Victor Guerra <victorbcguerra@gmail.com>
2020-06-14 18:07:15 +0100

    Improved to use redux

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -15,6 +15,13 @@ class StreamList extends React.Component {
       );
     }
   }
+
+const mapStateToProps = (state) => {
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn,
+  };
 };
 
-export default StreamList;
+export default connect(mapStateToProps, { fetchStreams })(StreamList);

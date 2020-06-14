4ef15a0f959113b16e8f2e145bde8301627eec3c
Victor Guerra <victorbcguerra@gmail.com>
2020-06-14 18:01:39 +0100

    Imported static types

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,14 @@
 import { SIGN_IN, SIGN_OUT } from "./types";
+import history from "../history";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from "./types";
 
 export const signIn = (userId) => {
   return {

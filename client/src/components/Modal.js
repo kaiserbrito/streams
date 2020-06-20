8345823102c986a1452866bb2cf2c332024ec428
Victor Guerra <victorbcguerra@gmail.com>
2020-06-20 18:00:52 +0100

    Defined a Modal reusable component

---
+++ client/src/components/Modal.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Modal = (props) => {
+  // Portal to be use when using modals - not rendered in #root id in html
+  return ReactDOM.createPortal(
+    <div onClick={props.onDismiss} className="ui dimmer modals visible active">
+      <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
+        <div className="header">{props.title}</div>
+        <div className="content">
+          <p>{props.content}</p>
+        </div>
+        <div className="actions">{props.actions}</div>
+      </div>
+    </div>,
+    document.querySelector("#modal")
+  );
+};
+
+export default Modal;
